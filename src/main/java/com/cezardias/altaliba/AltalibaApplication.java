c39e4e988394acad49aacf0805f2d12e2e608332
cezardias <cezar.dias@gmail.com>
2021-11-17 13:36:25 -0300

    second

---
+++ src/main/java/com/cezardias/altaliba/AltalibaApplication.java
@@ -0,0 +1,13 @@
+package com.cezardias.altaliba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltalibaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltalibaApplication.class, args);
+    }
+
+}
