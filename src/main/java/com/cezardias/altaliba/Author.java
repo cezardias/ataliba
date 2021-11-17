cc71cfcd0bb32da6f0ca53d7b68bd042b33c5f10
cezardias <cezar.dias@gmail.com>
2021-11-17 13:49:03 -0300

    Classe Author e book

--- src/main/java/com/cezardias/altaliba/Author.java
+++ src/main/java/com/cezardias/altaliba/Author.java
@@ -1,8 +1,18 @@
 package com.cezardias.altaliba;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Author {
 
     @Id
